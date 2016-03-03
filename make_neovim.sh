ab3ac916e28b9a3efedc4333dc74db1b5b48afb3
Bryce Fisher <bfisher@novetta.com>
2016-03-03 10:47:51 -0500

    Auto make neovim

---
+++ make_neovim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+sudo yum -y install libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip
+
+cd $HOME
+git clone git@github.com:neovim/neovim.git
+cd neovim
+make CMAKe_BUILD_TYPE=Release
+sudo make install
+
+mkdir -p $HOME/.config
+cd $HOME/.config
+ln -s $HOME/.vim nvim
+cd nvim
+ln -s $HOME/.vimrc init.vim

c83ba34c320138a8cf2a6e5b3a5830f73e0a3a28
Bryce Fisher <bfisher@novetta.com>
2016-03-03 10:49:16 -0500

    Use https nvim endpoint instead of ssh

--- make_neovim.sh
+++ make_neovim.sh
@@ -5,7 +5,7 @@ set -e
 sudo yum -y install libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip
 
 cd $HOME
-git clone git@github.com:neovim/neovim.git
+git clone https://github.com/neovim/neovim.git
 cd neovim
 make CMAKe_BUILD_TYPE=Release
 sudo make install
